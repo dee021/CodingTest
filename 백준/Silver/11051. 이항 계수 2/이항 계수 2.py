73422b6b473a69f4bec9bbdd88a1c7c33e7bf247
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-06-25 22:01:14 +0900

    [Silver III] Title: 이항 계수 2, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,16 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+
+num = 1
+den = 1
+
+for j in range(k,0,-1):
+    den = den * j
+
+while k:
+    num = num * n
+    n = n - 1
+    k = k -1
+
+c = num//den
+print(c%10007)
