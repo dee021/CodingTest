3e8a4ebaf9eaf2e2a0ab8bde67400bd70b5a7004
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-07-08 16:07:56 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 264 ms, Memory: 40596 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,16 +1,11 @@
 import sys
-from collections import deque
+from itertools import accumulate
 
 n, m = map(int, sys.stdin.readline().split())
 
-num = deque(list(map(int, sys.stdin.readline().split())))
-s = deque([0 for _ in range(n)])
-s[0] = num[0]
-
-for i in range(1, n):
-    s[i] = s[i-1] + num[i]
-
+num = list(map(int, sys.stdin.readline().split()))
+num = list(accumulate(num))
 
 for i in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    print(s[b-1] - s[a-2]) if a > 1 else print(s[b-1])
+    print(num[b-1]-num[a-2]) if a != 1 else print(num[b-1])
