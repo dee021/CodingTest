8df0415593813ef74656c1a9d40c410b6f5f0140
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-07-10 02:52:40 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,17 +1,10 @@
+def count(num):
+    n = 1
+    cnt = 0
+    while num >= (5**n):
+        cnt += num//(5**n)
+        n += 1
+    return cnt
+
 k = int(input())
-
-res = 1
-for i in range(k, 0, -1):
-    res = res * i
-
-res = list(str(res))
-res.reverse()
-
-zero = 0
-for i in res:
-    if i == '0':
-        zero = zero +1
-    else:
-        break
-
-print(zero)
+print(count(k))
