a9bf9eca5a587471ec843683328623b8d5f2805e
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-07-13 10:08:56 +0900

    [Bronze V] Title: 사칙연산, Time: 152 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+let input;
+let arr = [];
+
+rl.on('line', function(line) {
+    input = line;   
+    rl.close();
+}).on("close", () => {
+    arr = input.split(' ').map(el => parseInt(el));
+    console.log(arr[0]+arr[1]);
+    console.log(arr[0]-arr[1]);
+    console.log(arr[0]*arr[1]);
+    console.log(parseInt(arr[0]/arr[1]));
+    console.log(arr[0]%arr[1]);
+    process.exit();
+});
