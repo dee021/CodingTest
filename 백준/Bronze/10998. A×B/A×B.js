a4a40c3e19cbd620af630becbd0061a8cae1986d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-07-13 10:04:17 +0900

    [Bronze V] Title: A×B, Time: 148 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+let input;
+let arr = [];
+
+rl.on('line', function(line) {
+    input = line;   
+    rl.close();
+}).on("close", () => {
+    arr = input.split(' ').map(el => parseInt(el));
+    console.log(arr[0]*arr[1]);
+    process.exit();
+});
