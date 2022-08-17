0ee9e417a8711d8b800b0fb50e1a52aa4121adb5
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-08-17 17:18:43 +0900

    [Bronze II] Title: 막대기, Time: 120 ms, Memory: 34692 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,18 +1,10 @@
 import sys
-from collections import deque
-stick = deque(); maxV = 0
-for i in range(int(sys.stdin.readline())):
-    num = int(sys.stdin.readline())
-    maxV = max(maxV, num)
-    if num == maxV:
-        stick = deque([num])
-    else:
-        stick.appendleft(num)
+maxV = 0;cnt = 0
+stick = [int(sys.stdin.readline()) for _ in range(int(input()))]
 
-maxV = stick[0];cnt = 1
-for i in range(1, len(stick), 1):
-    if stick[i] > maxV:
+for i in reversed(stick):
+    if i > maxV:
         cnt += 1
-        maxV = stick[i]
+        maxV = i
 
 print(cnt)
