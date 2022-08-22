19a65299fcc0306c5155c32319d75af978a6bbaf
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-08-22 16:27:27 +0900

    [Silver V] Title: 이름 궁합, Time: 1584 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.py
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.py
@@ -8,6 +8,6 @@ for idx, val in enumerate(list(map(numName, list(input())))):
     dp.insert(idx*2+1, val)
 while len(dp) != 2:
     for i in range(len(dp)-1):
-        dp[i] = int(str(dp[i] + dp[i+1])[-1])
+        dp[i] = (dp[i] + dp[i+1])%10
     dp.pop()
 print(*dp, sep='')
