388163a695bb8cd60d7950c759fbd4c99680165d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-11-11 10:08:35 +0900

    [Bronze I] Title: 세로읽기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,5 +1,5 @@
-s = ['' for _ in range(5*15)]
-for i in range(5):
-    for idx, c in enumerate(input()):
-        s[5*idx+i] = c
-print(*[x for x in s if x], sep='')
+r = [input() for _ in range(5)]
+
+for j in range(15):
+    for i in range(5):
+        if j < len(r[i]): print(r[i][j], end='')
