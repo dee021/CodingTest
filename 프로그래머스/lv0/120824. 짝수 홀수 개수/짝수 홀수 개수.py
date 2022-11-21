77fee7f88ed9dad710057dbcdf631394572ab739
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-11-21 17:18:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,9 +1,5 @@
 def solution(num_list):
-    odd, even = 0, 0
+    answer = [0, 0]
     for x in num_list:
-        if x%2:
-            odd += 1
-        else:
-            even += 1
-    answer = [even, odd]
+        answer[x%2] += 1
     return answer
