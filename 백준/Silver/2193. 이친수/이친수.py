e7754ace28e9b7507862988a412d80a1737a483a
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-11-22 19:47:51 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,9 +1,7 @@
-arr = [[0,0] for _ in range(90)]
-arr[0] = [0, 1]
-arr[1] = [1, 0]
+arr = [0 for _ in range(90)]
+arr[0] = arr[1] = 1
 n = int(input())
 
 for i in range(2, n, 1):
-    arr[i][0] = sum(arr[i-1])
-    arr[i][1] = arr[i-1][0]
-print(sum(arr[n-1]))
+    arr[i] = arr[i-1] + arr[i-2]
+print(arr[n-1])
