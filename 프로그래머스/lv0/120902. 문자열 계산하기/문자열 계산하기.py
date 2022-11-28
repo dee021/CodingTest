b51fba1b09d18b4469030ac8976943fb0b001dcb
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-11-28 10:50:40 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    oper = list(my_string.split())
-    answer = int(oper.pop(0))
-    for i in range(0, len(oper), 2):
-        if oper[i] == '+':
-            answer += int(oper[i+1])
-        else: answer -= int(oper[i+1])
-    return answer
+    return eval(my_string)
