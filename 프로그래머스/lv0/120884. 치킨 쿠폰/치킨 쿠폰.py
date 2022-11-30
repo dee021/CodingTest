e725d7a6bbc985c4ee087444de5d9851910514d8
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-11-30 10:40:02 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -1,9 +1,8 @@
 def solution(chicken):
     def order(n):
         return n//10, n//10 + n%10
-    answer = 0; 
     chicken, coupon = order(chicken)
-    answer += chicken
+    answer = chicken
     while coupon >= 10:
         chicken, coupon = order(coupon)
         answer += chicken
