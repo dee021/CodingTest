cb3c78869de2ec737372f6e680744065ae0ee6f6
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-12-14 11:34:34 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,13 @@
+def solution(ingredient):
+    answer = 0
+    i = 0
+    
+    while i < len(ingredient) -3:
+        if ingredient[i:i+4] == [1,2,3,1]:
+            answer += 1
+            del ingredient[i:i+4]
+            i -= 3
+        else:
+            i += 1
+
+    return answer
