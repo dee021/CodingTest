c6547362e48b634cb310bfc5d5c16c8abb5fd277
Haneul Lee <80146182+dee021@users.noreply.github.com>
2022-12-29 02:46:37 +0900

    [Bronze III] Title: 상금 헌터, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/15953. 상금 헌터/상금 헌터.py
+++ 백준/Bronze/15953. 상금 헌터/상금 헌터.py
@@ -8,14 +8,14 @@ for _ in range(int(input())):
     ans = 0
     idx = 0
     a, b = map(int, input().split())
-    if a <= 21 and a:
+    if a:
         for i in range(len(ar)):
             idx += i
             if a <= idx:
                 ans += ar[i]
                 break
     idx = 0
-    if b <= 31 and b:
+    if b:
         for i in range(len(br)):
             idx += 2**i
             if b <= idx:
