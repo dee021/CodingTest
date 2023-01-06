80f42fcbf31fbebed2774e6243f16a05c84ae14d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-01-06 15:19:34 +0900

    [Bronze II] Title: 찾았다 악질, Time: 836 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/25576. 찾았다 악질/찾았다 악질.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+
+for _ in range(n-1):
+    tmp = 0
+    for i, k in enumerate(list(map(int, input().split()))):
+        tmp += abs(k-l[i])
+    if tmp > 2000:
+        print('YES')
+        exit(0)
+print('NO')
