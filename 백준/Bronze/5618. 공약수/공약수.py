d455cf65c574bbc3dcaf330d318e8fab7d3d5350
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-01-07 20:53:15 +0900

    [Bronze III] Title: 공약수, Time: 1352 ms, Memory: 117980 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.py
+++ 백준/Bronze/5618. 공약수/공약수.py
@@ -4,5 +4,4 @@ import math
 input()
 gcd = list(map(int, input().split()))
 gcd = reduce(lambda x, y: math.gcd(x, y), gcd)
-p = [x for x in range(1, gcd+1) if gcd%x == 0]
-print(*p, sep='\n')
+print(*[x for x in range(1, gcd+1) if gcd%x == 0], sep='\n')
