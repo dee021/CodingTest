6e622ec20ff57723241ff60e06155ab50693d6c0
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-01-11 10:14:58 +0900

    [Bronze II] Title: 거리의 합, Time: 468 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.py
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -1,6 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 ans = 0
 n = int(input())
 arr = list(map(int, input().split()))
-for i in arr:
-    ans += sum([abs(i-x) for x in arr])
-print(ans)
+for i in range(n-1):
+    for j in range(i+1, n):
+        ans += abs(arr[i]-arr[j])
+print(ans*2)
