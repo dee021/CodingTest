9758f1e48bdeb094d39198fbb6beec909c7bc736
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-02-01 15:45:31 +0900

    [Silver V] Title: 복불복으로 지구 멸망, Time: 84 ms, Memory: 34736 KB -BaekjoonHub

--- 백준/Silver/17358. 복불복으로 지구 멸망/복불복으로 지구 멸망.py
+++ 백준/Silver/17358. 복불복으로 지구 멸망/복불복으로 지구 멸망.py
@@ -1,3 +1,3 @@
 from functools import reduce
 n = int(input())
-print(reduce(lambda x, y:x*y, [x for x in range(n) if x%2])%(10**9+7))
+print(reduce(lambda x, y:x*y%1000000007, [x for x in range(n) if x%2]))
