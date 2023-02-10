bff7d34baa6a8548f125a7052167eb223f3bd31d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-02-10 10:21:15 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,9 +1,7 @@
 r, b = map(int, input().split())
 t = r + b
 for i in range(int(r/2), 2, -1):
-    for j in range(1, i+1):
-        if i * j > t:
-            break
-        if i * j == t and 2*(i+j)-4 == r:
-            print(i, j)
-            exit(0)
+    j = t/i
+    if 2*(i+j)-4 == r:
+        print(i, int(j))
+        break
