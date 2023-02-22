4971c93b1de9c2863ad0cd0cc56d380f4c3d009e
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-02-23 04:51:26 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 1100 ms, Memory: 147496 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -1,18 +1,10 @@
 import sys
-import math
 input = sys.stdin.readline
 
 p = int(input())
 arr = list(map(int, input().split()))
 k = int(input())
-if k == 1:
-    print(*sorted(arr))
-    exit(0)
-for l in range(1, int(math.log2(p))+1):
-    s = 0
-    for f in range(0, len(arr), 2**l):
-        s += 1
-        arr[f:f+2**l] = sorted(arr[f:f+2**l])
-    if k == s:
-        break
+dis = p//k
+for f in range(0, len(arr), dis):
+    arr[f:f+dis] = sorted(arr[f:f+dis])
 print(*arr)
