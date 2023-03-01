b2133a39221c41ae5a12b34b574995f907f31a9e
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-03-01 23:31:55 +0900

    [Bronze III] Title: 수학적 호기심, Time: 1412 ms, Memory: 31268 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,9 +1,16 @@
-from itertools import combinations as cb
+ans = [([-1] if i > 2 else [0])*101 for i in range(101)]
+
+def sol(n, m):
+    if ans[n][m] != -1:
+        return ans[n][m]
+    if n < 3:
+        return 0
+    ans[n][m] = sol(n-1, m)
+    a = n-1
+    for b in range(1, a):
+        ans[n][m] += bool((a**2+b**2+m) % (a*b) == 0)
+    return ans[n][m]
 
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    ans = 0
-    for a, b in list(cb([x for x in range(1, n)], 2)):
-        if (a**2+b**2+m) % (a*b) == 0:
-            ans += 1
-    print(ans)
+    print(sol(n, m))
