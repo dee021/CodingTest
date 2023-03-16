729b0071245faa659fea589f66bc807e9fa9deaf
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-03-16 16:16:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,11 +1,9 @@
-def sol(s, l, dep):
-    if s == '1':
-        return dep, l
-    tmp = len(s)
-    s = s.replace('0','')
-    l += tmp - len(s)
-    s = bin(len(s))[2:]
-    return sol(s, l, dep+1)
-
 def solution(s):
-    return sol(s, 0, 0)
+    l , d = 0, 0
+    while s != '1':
+        d += 1
+        tmp = len(s)
+        s = s.replace('0','')
+        l += tmp - len(s)
+        s = bin(len(s))[2:]
+    return [d, l]
