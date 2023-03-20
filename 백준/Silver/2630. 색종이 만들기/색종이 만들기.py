1fe43cee994023859b1f76830d984357e75eb21d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-03-20 15:07:21 +0900

    [Silver II] Title: 색종이 만들기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,30 +1,32 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+n = int(input())
 white, blue = 0, 0
 p = []
 
 for i in range(n):
-    p.extend(list(map(int, sys.stdin.readline().split())))
+    p.extend(list(map(int, input().split())))
 
 def sol(lst):
+    def divide():
+        one = list(); two = list(); three = list(); four = list()
+        for j in range(0, m*m//2, m):
+            one.extend(lst[j:j+size])
+            two.extend(lst[j+size:j+(size*2)])
+            three.extend(lst[m*m//2+j:m*m//2+j+size])
+            four.extend(lst[m*m//2+j+size:m*m//2+j+(size*2)])
+        return [one, two, three, four]
     global white, blue
     m = int(len(lst)**0.5)
     size = m//2
-    if sum(lst) == len(lst):
-        blue += 1
-    elif sum(lst) == 0:
-        white += 1
-    else: # 분할
-        def divide():
-            one = list(); two = list(); three = list(); four = list()
-            for j in range(0, m*m//2, m):
-                one.extend(lst[j:j+size])
-                two.extend(lst[j+size:j+(size*2)])
-            for j in range(m*m//2, m*m, m):
-                three.extend(lst[j:j+size])
-                four.extend(lst[j+size:j+(size*2)])
-            return [one, two, three, four]
+    s = set(lst)
+    if len(s) == 1:
+        if 0 in lst:
+            white += 1
+        else:
+            blue += 1
+    else:
         lst = divide()
         lst = list(map(sol, lst))
     return white, blue
