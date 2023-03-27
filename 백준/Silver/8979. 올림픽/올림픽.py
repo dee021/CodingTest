04e3c38f2beb4ee17a6492ffb6e7cacb39f8b602
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-03-27 14:44:52 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,6 @@
+n, k = map(int, input().split())
+m = [[] for _ in range(n)]
+for _ in range(n):
+    c, *t = map(int, input().split())
+    m[c-1] = t
+print(sorted(m, reverse=True).index(m[k-1])+1)
