0af635919b899da136a32e722a58351e49b61346
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-04-30 13:29:35 +0900

    [Silver II] Title: 제곱수의 합, Time: 3512 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -4,5 +4,6 @@ dp = [0 for _ in range(n+1)]
 for i in range(1,n+1):
     dp[i] = 1+dp[i-1]
     for j in range(2, int(i**0.5)+1, 1):
-        dp[i] = min(dp[i], 1+dp[i-j*j])
+        if dp[i] > 1 + dp[i-j*j]:
+            dp[i] = 1 + dp[i-j*j]
 print(dp[n])
