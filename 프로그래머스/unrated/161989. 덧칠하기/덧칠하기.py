d8ab3b0a8f237810a72d3cdba5c7bba9b6144fd8
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-05-09 01:33:10 +0900

    [unrated] Title: 덧칠하기, Time: 6.97 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -3,17 +3,11 @@ def solution(n, m, section):
     l = len(section)
     idx = 0
     while idx < l:
-        f = 1
-        min_range = section[idx]
+        answer += 1
+        ran = range(section[idx], section[idx]+m)
         for i in range(idx, l):
-            if f and section[i] in range(min_range, min_range+m):
-                f = 0
-                answer += 1
-            if section[i] not in range(min_range, min_range+m):
+            if section[i] not in ran:
                 idx = i
                 break
             if i == l - 1:
-                end = 1
-        if end:
-            break
-    return answer
+                return answer
