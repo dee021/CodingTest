59f412d2be147c380b179e28c3281e7032e0f6d4
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-05-14 18:21:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.33 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -5,19 +5,17 @@ def solution(str1, str2):
     def makeSet(t, l):
         t = t.lower()
         s = []
-        p = re.compile('[a-zA-Z]{2}')
+        p = re.compile('[a-z]{2}')
         for i in range(l-1):
             try:
                 s.append(p.match(t[i:i+2]).group())
             except:
                 pass
         return s
-    str1, str2 = makeSet(str1, len(str1)), makeSet(str2, len(str2))
-    if not str1 and not str2:
-        return 65536
-    str1, str2 = map(Counter, [str1, str2])
+    str1 = Counter(makeSet(str1, len(str1)))
+    str2 = Counter(makeSet(str2, len(str2)))
     c, p = 0, 0
     for w in set(str1.keys()) | set(str2.keys()):
         c += min(str1[w], str2[w])
         p += max(str1[w], str2[w])
-    return int(65536*c/p)
+    return int(65536*c/p) if p else 65536
