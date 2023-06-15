0c20dde2e5dbca35eb0225667eb36448162b05ac
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-06-15 21:28:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 1336 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,14 +1,8 @@
 n = int(input())
+arr = []; k = 666
 
-y = n//20 * 10000
-if n<50:
-    y = n * 10000
-elif n<500:
-    y = y + 0.15 *y
-elif n < 5000:
-    pass
-else:
-    y = 0.6 * y
-
-num = [x for x in range(666, int(y)) if '666' in str(x)]
-print(num[n-1])
+while len(arr) < n:
+    if str(k).count('666'):
+        arr.append(k)
+    k += 1
+print(arr[n-1])
