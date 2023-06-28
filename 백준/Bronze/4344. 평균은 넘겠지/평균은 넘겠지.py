c68043f3424a5cc76aaaa52afe38e8585238120c
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-06-28 10:22:17 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,12 +1,5 @@
-n=int(input())
-for i in range(n):
-    temp=input().split()
-    temp.reverse()
-    size=int(temp.pop())
-    count=0;
-    for j in range(size):
-        temp[j]=int(temp[j])
-    mean=sum(temp)/size
-    count = [1 for j in range(size) if temp[j]> mean]
-    rate = len(count)/size*100
-    print('%0.3f' %rate + '%')
+for _ in range(int(input())):
+    n, *score = map(int, input().split())
+    mean=sum(score)/n
+    ans = len(list(filter(lambda x: x>mean, score)))/n*100
+    print('%0.3f' %ans + '%')
