4b20099a71cb8d215806a06dced417cc8beca745
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-08-02 14:11:44 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,8 +1,6 @@
+sol = lambda i: s[i:i+pl] == pn
+
 pn = 'O'.join(['I']*(int(input())+1))
 sl, pl = int(input()), len(pn)
 s = input()
-ans = 0
-for i in range(sl-len(pn)+1):
-    if s[i:i+pl] == pn:
-        ans += 1
-print(ans)
+print(sum(list(map(sol, range(sl-len(pn)+1)))))
