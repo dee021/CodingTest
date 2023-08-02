3acb41f1d5d81d0cdd8d99cc485ba7531610468d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-08-02 12:31:09 +0900

    [Bronze II] Title: Affine Cipher, Time: 132 ms, Memory: 33460 KB -BaekjoonHub

--- 백준/Bronze/11575. Affine Cipher/Affine Cipher.py
+++ 백준/Bronze/11575. Affine Cipher/Affine Cipher.py
@@ -1,5 +1,6 @@
-e = lambda x: ((a*ord(x)-ord('A') + b) % 26) + ord('A')
-
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    print(''.join(list(map(chr, list(map(e, input()))))))
+    bf = ''.join([chr(i+ord('A')) for i in range(26)])
+    at = ''.join([bf[(a*x + b) % 26] for x in range(26)])
+    table = str.maketrans(bf, at)
+    print(input().translate(table))
