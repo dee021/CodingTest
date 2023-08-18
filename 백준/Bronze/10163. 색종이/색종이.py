a069b8f041736cf27d3767079fb4ab74825b009c
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-08-18 17:26:47 +0900

    [Bronze I] Title: 색종이, Time: 4584 ms, Memory: 123012 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -4,7 +4,7 @@ for k in range(1, n+1):
     x, y, w, h = map(int, input().split())
     for i in range(x, x+w):
         for j in range(y, y+h):
-            arr[i][j] = n
+            arr[i][j] = k
 
 cnt = [arr[i].count for i in range(1001)]
 for k in range(1, n+1):
