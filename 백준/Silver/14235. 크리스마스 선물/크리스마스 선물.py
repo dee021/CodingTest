7373979807c98ec77bcea7cd7a217b4a93524bab
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-10-12 20:56:02 +0900

    [Silver III] Title: 크리스마스 선물, Time: 408 ms, Memory: 40920 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -0,0 +1,11 @@
+import heapq
+
+n = int(input())
+gift = []
+for _ in range(n):
+    m, *t = list(map(int, input().split()))
+    if not m:
+        print(-heapq.heappop(gift) if gift else -1)
+    else:
+        for g in t:
+            heapq.heappush(gift, -g)
