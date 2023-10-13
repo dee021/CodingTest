94461f520a042fe417026792e44f4f8f1a574f07
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-10-13 15:42:24 +0900

    [Silver II] Title: 키로거, Time: 940 ms, Memory: 43220 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+for _ in range(int(input())):
+    p, w = [], deque() # pw = p + w
+    for o in input().strip():
+        if o.isalpha() or o.isnumeric():
+            p.append(o)
+        elif p and o == '-':
+            p.pop()
+        elif p and o == '<':
+            w.appendleft(p.pop())
+        elif w and o == '>':
+            p.append(w.popleft())
+    print(''.join(p) + ''.join(w))
