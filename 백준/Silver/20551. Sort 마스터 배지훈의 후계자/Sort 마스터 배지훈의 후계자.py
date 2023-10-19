e68f477440d3e7d192d05ed7ff5e011c4eee73d3
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-10-19 19:07:58 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 464 ms, Memory: 60200 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
-import bisect
 
 n, m = map(int, input().split())
-nums = set()
-arr, ans = [], []
-for _ in range(n):
-    k = int(input())
-    nums.add(k)
-    arr.insert(bisect.bisect_left(arr, k), k)
+nums = [int(input()) for _ in range(n)]
+nums.sort()
+ans = {}
+for i in range(n):
+    if nums[i] not in ans:
+        ans[nums[i]] = i
 for _ in range(m):
-    k = int(input())
-    ans.append(str(bisect.bisect_left(arr, k)) if k in nums else '-1')
-sys.stdout.write('\n'.join(ans))
+    print(ans.get(int(input()), -1))
