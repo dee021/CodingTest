ff1d8cb4ee3780ef8da1a90a15c5a77f2099d931
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-10-21 17:25:16 +0900

    [Silver II] Title: 외판원 순회 2, Time: 692 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,18 +1,23 @@
-from itertools import permutations
+def sol(start, t):
+    global ans
+    if all(visited):
+        if w[start][0]:
+            return t + w[start][0]
+        return 10**7
+    if t > ans:
+        return 10**7
+    
+    p = 10**7
+    for i in range(1, n):
+        if not visited[i] and w[start][i]:
+            visited[i] = 1
+            p = min(p, sol(i, t+w[start][i]))
+            visited[i] = 0
+    return p
 
+ans = 10**7
 n = int(input())
+visited = [0 for _ in range(n)]
+visited[0] = 1
 w = [list(map(int, input().split())) for _ in range(n)]
-t = list(map(list, permutations(range(n-1), n-1)))
-ans = 1000000*10
-for lst in t:
-    lst.append(n-1)
-    s = n-1; res = 0
-    for v in lst:
-        if not w[s][v]:
-            break
-        res += w[s][v]
-        s = v
-    else:
-        if ans > res:
-            ans = res
-print(ans)    
+print(sol(0, 0))
