fef0efbb6ea7623ec86473cdae2888f2bbbc3241
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-11-04 13:16:30 +0900

    [Silver I] Title: 부등호, Time: 3456 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -7,12 +7,14 @@ def sol(cnt, res):
     if res:
         res += arr[cnt-1]
     for k in range(10):
-        if not res.count(str(k)) and (not cnt or eval(res+str(k))):
+        if nums[k] and (not cnt or eval(res+str(k))):
+            nums[k] = 0
             sol(cnt+1, res+str(k))
+            nums[k] = 1
 
 c = int(input())
 arr = input().split()
-nums = [x for x in range(10)]
+nums = [1 for _ in range(10)]
 ans = []
 sol(0, '')
 ans.sort()
