59f4362aa5a266ae3d39f2a3b2f5bfd00a5f582b
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-11-07 19:59:38 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 1028 ms, Memory: 113812 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -0,0 +1,17 @@
+def sol(res1, res2):
+    global ans
+    if res2 and abs(res1-res2) < ans:
+        ans = abs(res1-res2)
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = 1
+            sol(res1*arr[i][0], res2+arr[i][1])
+            visited[i] = 0
+
+ans = 1000000000
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+sol(1,0)
+print(ans)
