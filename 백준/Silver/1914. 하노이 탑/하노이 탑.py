9c86f9368569e87ba43d8b266c1f7019132c8ce3
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-12-04 15:16:10 +0900

    [Silver I] Title: 하노이 탑, Time: 804 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -7,17 +7,13 @@ def next_g(a, b):
             return i
 
 def sol(k, cur, target):
-    if d[cur][-1] == k:
+    if k == 1:
         print('{} {}\n'.format(cur, target))
-        d[target].append(d[cur].pop())
-        arr[k] = target
         return
     
     sol(k-1, cur, next_g(cur, target))
     print('{} {}\n'.format(cur, target))
-    d[target].append(d[cur].pop())
-    arr[k] = target
-    sol(k-1, arr[k-1], target)
+    sol(k-1, next_g(cur, target), target)
 
 n = int(input())
 dp = [0 for _ in range(n)]
@@ -26,9 +22,4 @@ for i in range(1, n):
     dp[i] = dp[i-1] * 2 + 1
 print(str(dp[n-1])+'\n')
 if n <= 20:
-    arr = [1 for x in range(n+1)]
-    arr1 = [x for x in range(n, 0, -1)]
-    arr2 = []
-    arr3 = []
-    d = {1:arr1, 2:arr2, 3:arr3}
     sol(n, 1, 3)
