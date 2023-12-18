cc4a3d2175efcb8b8ba2130bbcedab32cc18d0cc
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-12-18 19:31:29 +0900

    [Silver I] Title: 관중석, Time: 564 ms, Memory: 49288 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,14 +1,14 @@
 from math import gcd
 
-r1, r2 = map(int, input().split())
-arr = [[0 for _ in range(x)] for x in range(r2+1)]
-ans = 0
-for r in range(r1, r2+1):
-    for k in range(r):
-        g = gcd(r, k)
-        n, d = k//g, r//g
-        
-        if not arr[d][n]:
-            ans += 1
-            arr[d][n] = 1
-print(ans)
+def sol(r1, r2):
+    arr = [[0 for _ in range(x+1)] for x in range(r2+1)]
+    ans = 0
+    for r in range(r1, r2+1):
+        for k in range(1, r+1):
+            g = gcd(r, k)
+            n, d = k//g, r//g
+            if not arr[d][n]:
+                ans += 1
+                arr[d][n] = 1
+    return ans
+print(sol(*map(int, input().split())))
