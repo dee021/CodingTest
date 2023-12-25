b6f29731f3cf37e8f89f550b365540af96476ad9
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-12-25 14:30:58 +0900

    [Silver II] Title: 파닭파닭, Time: 868 ms, Memory: 178396 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.py
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -2,15 +2,18 @@ import sys
 input = sys.stdin.readline
 
 s, c = map(int, input().split())
-arr = [int(input()) for _ in range(s)]
-t = sum(arr)
-l, r = 1, max(arr)
+arr = []
+l, r, t = 1,0,0
+for _ in range(s):
+    arr.append(int(input()))
+    r = max(r, arr[-1])
+    t += arr[-1]
+
 while l <= r:
     mid = (l+r) // 2
-    cnt, last = 0, 0
+    cnt = 0
     for i in range(s):
         cnt += arr[i] // mid
-        last += arr[i] % mid
     if cnt < c:
         r = mid - 1
     else:
