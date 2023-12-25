547c5d1fc6b79c29ddf2409d7cba2ac1de6029ed
Haneul Lee <80146182+dee021@users.noreply.github.com>
2023-12-25 15:33:01 +0900

    [Silver II] Title: 최대 페이지 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
@@ -1,20 +1,7 @@
-def sol(idx, days, pages):
-    global ans
-    
-    if days > n:
-        return
-    
-    ans = max(ans, pages)
-    
-    for i in range(idx, m):
-        if not visited[i]:
-            visited[i] = 1
-            sol(i+1, days + ch[i][0], pages + ch[i][1])
-            visited[i] = 0
-
 n, m = map(int, input().split())
-ch = [list(map(int, input().split())) for _ in range(m)]
-visited = [0 for _ in range(m)]
-ans = 0
-sol(0,0,0)
-print(ans)
+dp = [0 for _ in range(n+1)]
+for i in range(m):
+    d, p = map(int, input().split())
+    for j in range(n, d-1, -1):
+        dp[j] = max(dp[j], dp[j-d] + p)
+print(dp[n])
