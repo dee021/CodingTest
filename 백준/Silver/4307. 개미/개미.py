5872900a38615aad36541a19ba75ede9556c6181
Haneul Lee <80146182+dee021@users.noreply.github.com>
2024-02-02 13:41:47 +0900

    [Silver I] Title: 개미, Time: 440 ms, Memory: 39996 KB -BaekjoonHub

--- 백준/Silver/4307. 개미/개미.py
+++ 백준/Silver/4307. 개미/개미.py
@@ -8,10 +8,4 @@ for _ in range(int(input())):
     for i in range(n):
         minV = max(minV, min(ants[i], l-ants[i]))
         maxV = max(maxV, ants[i], l-ants[i])
-        if i:
-            m = (ants[i]-ants[i-1])/2
-            maxV = max(maxV, ants[i-1] + m, l - ants[i] + m)
-        if i != n - 1:
-            m = (ants[i+1]-ants[i])/2
-            maxV = max(maxV, ants[i] + m, l - ants[i+1] + m)
     print(minV, maxV)
