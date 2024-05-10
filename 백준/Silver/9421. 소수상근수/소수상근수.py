355af9878155702a4ffffab2a2469bcb17139178
Haneul Lee <80146182+dee021@users.noreply.github.com>
2024-05-10 16:21:50 +0900

    [Silver I] Title: 소수상근수, Time: 884 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/9421. 소수상근수/소수상근수.py
+++ 백준/Silver/9421. 소수상근수/소수상근수.py
@@ -2,23 +2,17 @@ import sys
 print = sys.stdout.write
 
 def isAns(num):
-    global mem
     path = set()
     while num != 1 and num not in path:
-        if num in mem:
-            mem = mem.union(path)
-            return True
         path.add(num)
         temp = 0
         for i in str(num):
             temp += int(i)**2
         num = temp
     if num == 1:
-        mem = mem.union(path)
         return True
     return False
 
-mem = set()
 n = int(input())
 nums = [1 if x % 2 else 0 for x in range(n+1)]
 nums[1] = 0
