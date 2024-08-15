b2dd2df1c80e809c2179d59b168c83244f46ee25
Haneul Lee <80146182+dee021@users.noreply.github.com>
2024-08-15 20:04:49 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        int idx = 0;
+        while (n_str.charAt(idx) == '0')
+            idx += 1;
+        return n_str.substring(idx, n_str.length());
+    }
+}
