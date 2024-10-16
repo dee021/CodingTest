1bdec5869e73fbe83e6c691a83aac66db5ab5153
Haneul Lee <80146182+dee021@users.noreply.github.com>
2024-10-16 12:35:24 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 336 ms, Memory: 171004 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+arr = [0]+sorted(map(int, input().split()))
+arr.sort()
+for i in range(2, n+1):
+    arr[i] += arr[i-1]
+
+for _ in range(q):
+    l, r = map(int, input().split())
+    print(arr[r] - arr[l-1])
