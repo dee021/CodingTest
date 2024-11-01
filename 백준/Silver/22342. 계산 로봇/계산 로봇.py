484b0876bab7409e448db6149d4eaa42595b990d
Haneul Lee <80146182+dee021@users.noreply.github.com>
2024-11-01 19:43:24 +0900

    [Silver I] Title: 계산 로봇, Time: 560 ms, Memory: 175716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.py
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -3,19 +3,15 @@ input = sys.stdin.readline
 
 h, w = map(int, input().split())
 arr = [list(map(int, input().strip())) for _ in range(h)]
-dp = [[0 for _ in range(w-1)] for _ in range(h)]
+dp = [[0 for _ in range(w)] for _ in range(h)]
 
 for c in range(w-1):
     for r in range(h):
-        if c == 0:
-            dp[r][0] = arr[r][0]
-        else:
-            if r == 0:
-                dp[r][c] = max(dp[r][c-1], dp[r+1][c-1]) + arr[r][c]
-            elif r == h-1:
-                dp[r][c] = max(dp[r][c-1], dp[r-1][c-1]) + arr[r][c]
-            else:
-                dp[r][c] = max(dp[r-1][c-1], dp[r][c-1], dp[r+1][c-1]) + arr[r][c]
+        dp[r][c+1] = max(dp[r][c+1], dp[r][c] + arr[r][c])
+        if r:
+            dp[r-1][c+1] = max(dp[r-1][c+1], dp[r][c] + arr[r][c])
+        if r < h-1:
+            dp[r+1][c+1] = max(dp[r+1][c+1], dp[r][c] + arr[r][c])
 
 ans = 0
 for r in range(h):
