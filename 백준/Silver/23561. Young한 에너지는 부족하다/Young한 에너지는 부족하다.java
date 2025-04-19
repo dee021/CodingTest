137ed3cd1c14aacd96606b1488460d501099fcc1
Haneul Lee <80146182+dee021@users.noreply.github.com>
2025-04-19 14:54:01 +0900

    [Silver II] Title: Young한 에너지는 부족하다, Time: 380 ms, Memory: 61880 KB -BaekjoonHub

---
+++ 백준/Silver/23561. Young한 에너지는 부족하다/Young한 에너지는 부족하다.java
@@ -0,0 +1,20 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n*3];
+        
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n*3; i++)  arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+
+		System.out.println(arr[n*2-1] - arr[n]);
+		br.close();
+	}
+}
