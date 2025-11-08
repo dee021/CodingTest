1334dbf1698e3253de556665f6a23315ee5a8fde
Haneul Lee <80146182+dee021@users.noreply.github.com>
2025-11-08 15:20:32 +0900

    [Bronze III] Title: 아카라카 2, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append("AKARAKA");
+        
+        int n = Integer.parseInt(br.readLine()) -1;
+        
+        while (n-- > 0) {
+        	sb.append("RAKA");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
