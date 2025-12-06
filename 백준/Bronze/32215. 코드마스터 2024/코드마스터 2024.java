66d4306c344fd934b8d84b8a75ebbfed37149c9c
Haneul Lee <80146182+dee021@users.noreply.github.com>
2025-12-06 13:50:07 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,16 @@
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+        
+        System.out.println((k+1) * m);
+        br.close();
+    }
+}
