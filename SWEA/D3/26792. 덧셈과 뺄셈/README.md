45735a86f834aa785c278142f4dd9c109e276ad2
Haneul Lee <80146182+dee021@users.noreply.github.com>
2026-08-16 12:26:41 +0900

    [D3] Title: 덧셈과 뺄셈, Time: 74 ms, Memory: 25,728 KB -BaekjoonHub

---
+++ SWEA/D3/26792. 덧셈과 뺄셈/README.md
@@ -0,0 +1,15 @@
+# [D3] 덧셈과 뺄셈 - 26792 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ3XsaWKSB3HBIPV) 
+
+### 성능 요약
+
+메모리: 25,728 KB, 시간: 74 ms, 코드길이: 858 Bytes
+
+### 제출 일자
+
+2026-08-16 12:26
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/26792. 덧셈과 뺄셈/덧셈과 뺄셈.java
@@ -0,0 +1,30 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+        	st = new StringTokenizer(br.readLine(), " ");
+        	int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+    		sb.append(String.format("%d %d\n", (x+y)/2, (x-y)/2));
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
